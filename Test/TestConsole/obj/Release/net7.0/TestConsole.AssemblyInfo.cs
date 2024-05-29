19bdf3f2241aaa6f6ff29d7cad0fce9cc3699ffd
gogagi0603 <gogagi0603@naver.com>
2024-05-29 12:54:49 +0900

    [BUILD] DLL updated. Deploy by TeamCity

--- Test/TestConsole/obj/Release/net7.0/TestConsole.AssemblyInfo.cs
+++ Test/TestConsole/obj/Release/net7.0/TestConsole.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26ab383f63da4772f22bb2dff2895ae02d3372dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f43718c048809fb0c1f43dd3251310e3b97b71ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
